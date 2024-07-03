9e6e0892ce35f15075217213c3ce2acf8c7eec16
janvi-aroraa <jjanvi.arora18@gmail.com>
2024-07-03 15:33:16 +0530

    main screen setup

---
+++ LoginSignup/LoginTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginTableViewController.swift
+//  LoginSignup
+//
+//  Created by Janvi Arora on 03/07/24.
+//
+
+import UIKit
+
+class LoginTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /// To make the entire content int he center (especially for iPad)
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let tableViewHeight = self.tableView.frame.height
+        let contentHeight = self.tableView.contentSize.height
+
+        let centeringInset = (tableViewHeight - contentHeight) / 2.0
+        let topInset = max(centeringInset, 0.0)
+
+        self.tableView.contentInset = UIEdgeInsets(top: topInset, left: 0.0, bottom: 0.0, right: 0.0)
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        UIScreen.main.bounds.height
+    }
+
+}
