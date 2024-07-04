7b3a29217e3998ab9ee5a4ec5f72079c3674786a
janvi-aroraa <jjanvi.arora18@gmail.com>
2024-07-04 14:16:23 +0530

    fix

---
+++ Signup/SignupTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignupTableViewController.swift
+//  LoginSignup
+//
+//  Created by Janvi Arora on 04/07/24.
+//
+
+import UIKit
+
+class SignupTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
