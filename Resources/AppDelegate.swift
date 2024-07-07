3ffc90c3a4b02c71ada0a129e00cd0f4d66449cd
janvi-aroraa <jjanvi.arora18@gmail.com>
2024-07-07 11:43:50 +0530

    enhancement

--- Resources/AppDelegate.swift
+++ Resources/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
 
         ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
-        FirebaseApp.configure()
+//        FirebaseApp.configure()
 
         return true
     }
